ee8debe89d8cb360c588102b8f4e43361236ffc3
Sergio Perez <sergio13prez@gmail.com>
2019-08-26 01:43:33 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -42,17 +42,18 @@ end
 
 def hit? card_total
   # code hit? here
-  prompt_user #ask the user if they want to hit or stay
+  #ask the user if they want to hit or stay
   user_input = get_user_input #set a variable to use for your if/else statement
   if user_input == "h"
     card_total += deal_card #increase your former total by the new number given by dealing another card
   elsif user_input == "s"
     card_total #return the number and prompt user to hit again
   else
+    prompt_user
     invalid_command
   end
   
-  prompt_user
+
 end
 
 def invalid_command
